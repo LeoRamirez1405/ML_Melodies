87dc658dd4d84976e257ae331998367e5d91e81f
nao_lahera <lahera0103@gmail.com>
2024-07-02 23:48:08 -0400

    Modificacion .sh construccion del dataset

--- corpus/EXE-CORPUS-GUITAR.sh
+++ corpus/EXE-CORPUS-GUITAR.sh
@@ -28,15 +28,15 @@ python3 $CURRENT_DIR/corpus/rename_guitar.py -d_i $CURRENT_DIR/corpus/GUITAR-V0/
 
 # 4. convert wav to log-mel spectrogram
 mkdir -p $CURRENT_DIR/corpus/GUITAR-V0/feature
-python3 $CURRENT_DIR/corpus/conv_wav2fe.py -d_list $CURRENT_DIR/corpus/GUITAR-V0/list -d_mp3 $CURRENT_DIR/corpus/GUITAR-V0/mp3 -d_feature $CURRENT_DIR/corpus/GUITAR-V0/feature -config $CURRENT_DIR/corpus/config.json
+python3 $CURRENT_DIR/corpus/conv_wav2fe_guitar.py -d_list $CURRENT_DIR/corpus/GUITAR-V0/list -d_mp3 $CURRENT_DIR/corpus/GUITAR-V0/mp3 -d_feature $CURRENT_DIR/corpus/GUITAR-V0/feature -config $CURRENT_DIR/corpus/config.json
 
 # 5. convert midi to note
 mkdir -p $CURRENT_DIR/corpus/GUITAR-V0/note
-python3 $CURRENT_DIR/corpus/conv_midi2note.py -d_list $CURRENT_DIR/corpus/GUITAR-V0/list -d_midi $CURRENT_DIR/corpus/GUITAR-V0/midi -d_note $CURRENT_DIR/corpus/GUITAR-V0/note -config $CURRENT_DIR/corpus/config.json
+python3 $CURRENT_DIR/corpus/conv_midi2note_guitar.py -d_list $CURRENT_DIR/corpus/GUITAR-V0/list -d_midi $CURRENT_DIR/corpus/GUITAR-V0/midi -d_note $CURRENT_DIR/corpus/GUITAR-V0/note -config $CURRENT_DIR/corpus/config.json
 
 # 6. convert note to label
 mkdir -p $CURRENT_DIR/corpus/GUITAR-V0/label
-python3 $CURRENT_DIR/corpus/conv_note2label.py -d_list $CURRENT_DIR/corpus/GUITAR-V0/list -d_note $CURRENT_DIR/corpus/GUITAR-V0/note -d_label $CURRENT_DIR/corpus/GUITAR-V0/label -config $CURRENT_DIR/corpus/config.json
+python3 $CURRENT_DIR/corpus/conv_note2label_guitar.py -d_list $CURRENT_DIR/corpus/GUITAR-V0/list -d_note $CURRENT_DIR/corpus/GUITAR-V0/note -d_label $CURRENT_DIR/corpus/GUITAR-V0/label -config $CURRENT_DIR/corpus/config.json
 
 # 7. convert txt to reference for evaluation
 mkdir -p $CURRENT_DIR/corpus/GUITAR-V0/reference
