0caf20bed01fdd251e01edac306f9c2ed72549ae
LeoRamirez1405 <leojavramirez1405@gmail.com>
2024-07-06 19:30:14 -0400

    inferencia completa en un .sh

--- inference/AUDIO-TRANSCRIPTION.sh
+++ inference/AUDIO-TRANSCRIPTION.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 CURRENT_DIR=$(pwd)
-FILE_CONFIG=$CURRENT_DIR/corpus/MAESTRO-V3/dataset/config.json
-RUTA_MODELO=$CURRENT_DIR/checkpoint/MAESTRO-V3/model_016_003.pkl
+FILE_CONFIG=$CURRENT_DIR/corpus/GUITAR-V1/dataset/config.json
+RUTA_MODELO=$CURRENT_DIR/checkpoint/GUITAR-V1/model_016_003.pkl
 RUTA_SALIDA_MIDI=$CURRENT_DIR/inference/01.midi
 RUTA_AUDIO=$CURRENT_DIR/inference/01.mp3
+RUTA_CHART=$CURRENT_DIR/inference/
+
+
 python3 $CURRENT_DIR/inference/audio2midiIinference.py $RUTA_AUDIO $RUTA_MODELO $FILE_CONFIG $RUTA_SALIDA_MIDI
+python3 $CURRENT_DIR/inference/midi2chart.py --file $RUTA_SALIDA_MIDI --output $RUTA_CHART
--- inference/midi2chart.py
+++ inference/midi2chart.py
@@ -1,10 +1,13 @@
 from music21 import converter, midi, environment
+import sys
+sys.path.append("/mnt/c/Users/53527/Desktop/ML_Melodies")  # Asegúrate de que esta ruta sea correcta
+import argparse
 
 # Configurar la ruta correcta a MuseScore
 us = environment.UserSettings()
 us['musescoreDirectPNGPath'] = '/usr/bin/mscore3'  # Asegúrate de que esta ruta sea correcta
 
-def midi_to_score(midi_file_path):
+def midi_to_score(midi_file_path='./example.midi', output_image_path='./partitura.png'):
     try:
         # Cargar el archivo MIDI
         midi_data = midi.MidiFile()
@@ -14,21 +17,18 @@ def midi_to_score(midi_file_path):
 
         # Convertir a una partitura de music21
         score = converter.parse(midi_file_path)
-        return score
+        score.write('musicxml.png', fp=output_image_path)
+        print(f"Partitura guardada en {output_image_path}")
     except Exception as e:
         print(f"Error al convertir el archivo MIDI: {e}")
+        print("No se pudo generar la partitura.")
         return None
 
-# Ruta al archivo MIDI
-midi_file_path = './example.midi'
-output_image_path = './partitura.png'
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convertir archivo MIDI a partitura.')
+    parser.add_argument('--file', '-f', required=True, help='Ruta al archivo MIDI')
+    parser.add_argument('--output', '-o', default='./partitura.png', help='Ruta donde guardar la imagen de la partitura')
 
-# Convertir el archivo MIDI en una partitura
-score = midi_to_score(midi_file_path)
-
-if score:
-    # Guardar la partitura como imagen PNG
-    score.write('musicxml.png', fp=output_image_path)
-    print(f"Partitura guardada en {output_image_path}")
-else:
-    print("No se pudo generar la partitura.")
+    args = parser.parse_args()
+    print(f"{args.file}\n {args.output}")
+    midi_to_score(args.file, args.output)
